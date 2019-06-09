276b8aa769d3b1e5f8681264ee8cc47d116a2667
Alex Chen <aecchen@sas.upenn.edu>
2019-06-09 15:55:22 -0600

    added getters and setters

--- meal.py
+++ meal.py
@@ -11,9 +11,32 @@ class Meal:
         self.price = price
         self.category = category
         self.timestamp = timestamp
-        
-meal1 = Meal("$9.99", "Breakfast", "6/9/19")
     
-print(meal1.price)
+    def setPrice(self, price):
+        self.price = price
+    
+    def getPrice(self):
+        return self.price
+    
+    def setCategory(self, category):
+        self.category = category
+        
+    def getCategory(self):
+        return self.category
+    
+    def setTimestamp(self, timestamp):
+        self.timestamp = timestamp
+    
+    def getTimeStamp(self):
+        return self.timestamp
+    
+
+meal1 = Meal("9.99", "Breakfast", "6/9/19")
+        
+
+print(meal1.getPrice())
 print(meal1.category)
 print(meal1.timestamp)
+
+meal1.setCategory("Lunch")
+print(meal1.getCategory())
