d4075134da1aee3f5e1ab5d980fada4b64e0933d
Alex Chen <aecchen@sas.upenn.edu>
2019-07-04 22:26:17 -0600

    got precise dates index off by 1

--- angular/meal-tracker-frontend/src/app/services/stats.service.ts
+++ angular/meal-tracker-frontend/src/app/services/stats.service.ts
@@ -16,9 +16,9 @@ export class StatsService {
   }
 
   getMealsByTimeFrame(timeframe):Observable<any> {
-    return this.http.get(this.baseurl + "/filterbytime?start_date_month=" + timeframe.startDate.getMonth() + 
+    return this.http.get(this.baseurl + "/filterbytime?start_date_month=" + (timeframe.startDate.getMonth() + 1) + 
     "&start_date_day=" + timeframe.startDate.getDate() + "&start_date_year=" + timeframe.startDate.getFullYear() + 
-    "&end_date_month=" + timeframe.endDate.getMonth() + "&end_date_day=" + timeframe.endDate.getDate() +
+    "&end_date_month=" + (timeframe.endDate.getMonth() + 1) + "&end_date_day=" + timeframe.endDate.getDate() +
     "&end_date_year=" + timeframe.endDate.getFullYear());
   }
 }
